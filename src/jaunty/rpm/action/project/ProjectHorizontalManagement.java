3320ae4f6999dbe0902ace621551199a202a985e
hellojinjie1 <hellojinjie@163.com>
2010-12-02 20:29:06 +0800

    纵横项目

---
+++ src/jaunty/rpm/action/project/ProjectHorizontalManagement.java
@@ -0,0 +1,9 @@
+package jaunty.rpm.action.project;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ProjectHorizontalManagement extends ActionSupport {
+
+	private static final long serialVersionUID = 5388061199956120262L;
+
+}
---
+++ src/jaunty/rpm/action/project/ProjectVerticalManagement.java
@@ -0,0 +1,9 @@
+package jaunty.rpm.action.project;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ProjectVerticalManagement extends ActionSupport {
+
+	private static final long serialVersionUID = 8220251235825319078L;
+
+}
